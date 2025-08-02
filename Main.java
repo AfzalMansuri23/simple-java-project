553ae69657720db5d279ad162c88d01cb8884702
sonal04 <afzalmansuri233@gmail.com>
2025-08-03 00:39:01 +0530

    added extra line of code in Main.java file

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main{
     public static void main(String[] args){
         System.out.println("Hello from docker Java App");
+        System.out.println("This is a output from a Java App runnning inside a multistage container")
     
     }
 }
