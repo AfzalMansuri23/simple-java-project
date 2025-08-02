57ed9975337f5e90208d61ebb2dee23b09988c65
sonal04 <afzalmansuri233@gmail.com>
2025-08-03 00:16:45 +0530

    Fix typo: system -> System

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String[] args){
-        system.out.println("Hello from docker Java App");
+        System.out.println("Hello from docker Java App");
     
     }
 }
